b043b237044ce6351766903cf7fde3a39e0ff838
Lakshmi <lakshmi.vishwakarma@encora.com>
2023-12-08 15:14:07 +0530

    added navbar

--- hello.py
+++ hello.py
@@ -1,111 +1,24 @@
-msg="hello world"
-print(msg)
-name= "lakshmi"
-age=28
+from flask import Flask, render_template
 
-print(name)
+app = Flask(__name__)
+posts=[
+    {
+        "author":"joy",
+        "title":"run",
+        "content":"post1"
 
-# if 5 > 2:
-#   print("Five is greater than two!")
+    },
+      {
+        "author":"joy",
+        "title":"run",
+        "content":"post1"
 
-#   firstName="Tony"
-#   lastName="Strak"
-#   age=51
-#   isGenius=True
-#   name=input("what is your name")
-#   print("hello" +name)
-# the_world_is_flat = True
-# if the_world_is_flat:
-#     print("Be careful not to fall off!")
+    }
+]
+@app.route("/home")
+def home():
+    return render_template ("home.html",posts=posts)
 
-# i=2+5*3
-# print(i)
- 
-# age=20
-# if age>18:
-#     print("You can vote")
-# elif age <18 and age>3:
-#     print("You are in school")
-# else:
-#     print("you are kid")
-
-# numbers= range(5)
-# print(numbers)
-
-# i=1
-# while i<=5:
-#     print(i*"*")
-#     i=i+1
-# def sum(num1, num2):
-#     sum=num1+num2
-#     return sum
-
-# result= sum(40,50)
-# print(result)
-
-
-# # Print the sum of the current number and the previous number
-# print("Printing current and previous number sum in a range(10)")
-# number=range(1,11)
-# previousNum=0
-# for i in range(1, 11):
-#     sum= previousNum+i
-#     print("current number", i, "previous number",previousNum, "sum",sum)
-#     previousNum=i
-
-
-
-# # Print characters from a string that are present at an even index number
-# word = input('Enter word ')
-
-# print("Orginal String is: ", word)
-# print("Printing only even index chars")
-
-# x= list(word)
-# print(x)
-
-# for i in x[0::2]:
-#     print(i)
-
-
-# # Remove first n characters from a string
-# def removeChar(string, n):
-#     x= string[n:]
-#     print(x)
-#     return x
-
-# removeChar("lakshmi",2)
-
-
-# # Check if the first and last number of a list is the same
-# numbers_x = [10, 20, 30, 40, 10]
-# numbers_y = [75, 65, 35, 75, 30]
-
-# def checkList(n):
-#     if n[0]==n[-1]:
-#         return True
-#     else:
-#         return False
-    
-# print(checkList(numbers_x))
-# print(checkList(numbers_y))
-        
-# Return the count of a given substring from a string
-
-# str = "Emma is good developer. Emma is a writer"
-# subStr="Emma"
-
-# def count(str, subStr):
-   
-#     count = 0
-#     for i in range(len(str) - 1):
-#         print(i)
-#         count += str[i: i + 4] == subStr
-#     return count
-
-
-# print(count("Emma is good developer. Emma is a writer","Emma"))
-
-
-
-
+@app.route("/about")
+def about():
+    return render_template ("about.html",titel="about page")
